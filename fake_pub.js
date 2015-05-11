29e98cd7864f02fc9ec1d83e8d5d6579d40adc2d
swissw <pi@raspberrypi.(none)>
2015-05-11 14:04:16 +0000

    adpot json format

--- fake_pub.js
+++ fake_pub.js
@@ -1,5 +1,14 @@
 var nats = require('nats').connect();
+var speech_command = { "sensor" : "temp",
+                       "value" : 20 }
 
 // publish events
-nats.publish("humix.sense.speech.command","hello jeffrey");
+//nats.publish("humix.sense.speech.command","hello jeffrey");
+//nats.publish("humix.sense.speech.command","age/10");
+nats.publish("humix.sense.speech.command",JSON.stringify(speech_command));
+
+
+var speech_command = { "sensor" : "age",
+                       "value" : 38 }
+nats.publish("humix.sense.speech.command",JSON.stringify(speech_command));
 
