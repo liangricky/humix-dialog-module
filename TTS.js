3abb38bc42cfa17aa95d55da718e68e7a0cfb0db
swissw <pi@raspberrypi.(none)>
2015-05-12 10:04:54 +0000

    1. change voice base path 2.upload humid wav

--- TTS.js
+++ TTS.js
@@ -2,17 +2,15 @@ var console = require('console');
 var sys = require('sys');
 var nats = require('nats').connect();
 var exec = require('child_process').exec;
-var voice_path = "/home/pi/humix/humix-sense/controls/humix-sense-speech/voice/";
+var voice_path = "./voice/";
 function puts(error, stdout, stderr) {sys.puts(stdout)}
 
 var msg = '';
 // subscribe events
 nats.subscribe('humix.sense.speech.command', function(msg) {
-   //exec("flite -t \""+ msg + "\"", puts);
    speech_command = JSON.parse(msg);
    var wav_file = voice_path + speech_command.sensor + "/" + speech_command.value + ".wav";
-   exec("aplay "+ wav_file, puts);
+   exec("aplay "+ wav_file, null);
    console.log('Received a message: ' + msg);
    console.log('Play wav file: ' + wav_file);
 });
-//exec("flite -t \"hello world\"", puts);
