13fdfa9a74a4cf4b772fdac62faac88d06320f19
swissw <pi@raspberrypi.(none)>
2015-05-27 12:15:51 +0000

    1.upload sphinx tar file 2.modify file path in sp_run.sh

--- sp_run.sh
+++ sp_run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pocketsphinx_continuous -vad_threshold 3.5 -upperf 1000 -hmm /usr/local/share/pocketsphinx/model/en-us/en-us -lm /home/pi/humix/sphinx/knowledge-base/humix.lm -dict /home/pi/humix/sphinx/knowledge-base/humix.dic -samprate 16000 -inmic yes
+pocketsphinx_continuous -vad_threshold 4 -upperf 1000 -hmm /usr/local/share/pocketsphinx/model/en-us/en-us -lm /home/pi/humix/humix-sense/controls/humix-sense-speech/humix.lm -dict /home/pi/humix/humix-sense/controls/humix-sense-speech/humix.dic -samprate 16000 -inmic yes
