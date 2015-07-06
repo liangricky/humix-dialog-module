9e6734f26bfa56cfbe8767db16e426d8e25594b6
yhwang <yihongwang@gmail.com>
2015-07-07 00:00:04 +0800

    add Makefile and create a new main file

---
+++ Makefile
@@ -0,0 +1,52 @@
+SPHINXBASE_VER=5prealpha
+POCKETSPHINX_VER=5prealpha
+OBJ_SPHINXBASE=deps/sphinxbase-$(SPHINXBASE_VER)/src/libsphinxbase/.libs/libsphinxbase.a
+OBJ_SPHINXAD=deps/sphinxbase-$(SPHINXBASE_VER)/src/libsphinxad/.libs/libsphinxad.a
+OBJ_POCKETSPHINX=deps/pocketsphinx-$(POCKETSPHINX_VER)/src/libpocketsphinx/.libs/libpocketsphinx.a
+OBJ_HUMIXSPEECH=humix-speech.o
+CFLAGS=-g -O2 -Wall
+
+all: sphinxbase sphinxad pocketsphinx humix-speech
+
+
+.PHONY: sphinxbase 
+sphinxbase:  $(OBJ_SPHINXBASE)
+
+.PHONY: sphinxad
+sphinxad: $(OBJ_SPHINXAD)
+
+.PHONY: pocketsphinx
+pocketsphinx: $(OBJ_POCKETSPHINX)
+
+#		-Wl,--start-group $(OBJ_SPHINXBASE) $(OBJ_SPHINXAD) $(OBJ_POCKETSPHINX) -lasound -lm -Wl,--end-group \
+#
+humix-speech: $(OBJ_HUMIXSPEECH)
+	gcc $(CFLAGS) \
+		-Ldeps/sphinxbase-$(SPHINXBASE_VER)/src/libsphinxbase/.libs \
+		-Ldeps/sphinxbase-$(SPHINXBASE_VER)/src/libsphinxad/.libs \
+		-Ldeps/pocketsphinx-$(POCKETSPHINX_VER)/src/libpocketsphinx/.libs \
+		-rdynamic -Wl,--whole-archive $(OBJ_SPHINXBASE) $(OBJ_SPHINXAD) $(OBJ_POCKETSPHINX) -Wl,--no-whole-archive  \
+		-o $@ -Wl,--start-group $(OBJ_HUMIXSPEECH) $(OBJ_SPHINXBASE) $(OBJ_SPHINXAD) $(OBJ_POCKETSPHINX) -Wl,--end-group \
+		-lasound -lm
+
+$(OBJ_HUMIXSPEECH): sphinxbase sphinxad pocketsphinx
+	gcc $(CFLAGS) -I. -Ideps/sphinxbase-$(SPHINXBASE_VER)/include -Ideps/pocketsphinx-$(POCKETSPHINX_VER)/include -lasound -lpthread -lm -c -o $@ humix-speech.c
+
+$(OBJ_SPHINXBASE): deps
+	tar -xf sphinxbase-$(SPHINXBASE_VER).tar.gz -C deps
+	cd deps/sphinxbase-$(SPHINXBASE_VER); ./configure --enable-fixed
+	make -C deps/sphinxbase-$(SPHINXBASE_VER) all
+
+$(OBJ_SPHINXAD): $(OBJ_SPHINXBASE)
+
+$(OBJ_POCKETSPHINX): deps
+	tar -xf pocketsphinx-$(POCKETSPHINX_VER).tar.gz -C deps
+	cd deps/pocketsphinx-$(POCKETSPHINX_VER); ./configure
+	make -C deps/pocketsphinx-$(POCKETSPHINX_VER) all
+
+
+deps:
+	mkdir deps
+
+clean:
+	rm -rf deps $(OBJ_HUMIXSPEECH) humix-speech
--- humix-speech.c
+++ humix-speech.c
@@ -1,56 +1,3 @@
-/* -*- c-basic-offset: 4; indent-tabs-mode: nil -*- */
-/* ====================================================================
- * Copyright (c) 1999-2010 Carnegie Mellon University.  All rights
- * reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * This work was supported in part by funding from the Defense Advanced 
- * Research Projects Agency and the National Science Foundation of the 
- * United States of America, and the CMU Sphinx Speech Consortium.
- *
- * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND 
- * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
- * NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * ====================================================================
- *
- */
-/*
- * continuous.c - Simple pocketsphinx command-line application to test
- *                both continuous listening/silence filtering from microphone
- *                and continuous file transcription.
- */
-
-/*
- * This is a simple example of pocketsphinx application that uses continuous listening
- * with silence filtering to automatically segment a continuous stream of audio input
- * into utterances that are then decoded.
- * 
- * Remarks:
- *   - Each utterance is ended when a silence segment of at least 1 sec is recognized.
- *   - Single-threaded implementation for portability.
- *   - Uses audio library; can be replaced with an equivalent custom library.
- */
-
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
@@ -60,16 +7,10 @@
 #include <errno.h>
 #include <sys/wait.h>
 
-#if defined(_WIN32) && !defined(__CYGWIN__)
-#include <windows.h>
-#else
 #include <sys/select.h>
-#endif
-
 #include <sphinxbase/err.h>
 #include <sphinxbase/ad.h>
-
-#include "pocketsphinx.h"
+#include <pocketsphinx.h>
 
 static const arg_t cont_args_def[] = {
     POCKETSPHINX_OPTIONS,
@@ -82,14 +23,6 @@ static const arg_t cont_args_def[] = {
      ARG_STRING,
      NULL,
      "Name of audio device to use for input."},
-    {"-infile",
-     ARG_STRING,
-     NULL,
-     "Audio file to transcribe."},
-    {"-inmic",
-     ARG_BOOLEAN,
-     "no",
-     "Transcribe audio from microphone."},
     {"-time",
      ARG_BOOLEAN,
      "no",
@@ -120,123 +53,16 @@ static const arg_t cont_args_def[] = {
 
 static ps_decoder_t *ps;
 static cmd_ln_t *config;
-static FILE *rawfd;
 static char const* cmdproc = NULL;
 static char const* wav_say = NULL;
 static char const* wav_proc = NULL;
 static char const* wav_bye = NULL;
 static char const* lang = NULL;
 
-static void
-print_word_times()
-{
-    int frame_rate = cmd_ln_int32_r(config, "-frate");
-    ps_seg_t *iter = ps_seg_iter(ps, NULL);
-    while (iter != NULL) {
-        int32 sf, ef, pprob;
-        float conf;
-
-        ps_seg_frames(iter, &sf, &ef);
-        pprob = ps_seg_prob(iter, NULL, NULL, NULL);
-        conf = logmath_exp(ps_get_logmath(ps), pprob);
-        printf("%s %.3f %.3f %f\n", ps_seg_word(iter), ((float)sf / frame_rate),
-               ((float) ef / frame_rate), conf);
-        iter = ps_seg_next(iter);
-    }
-}
-
-static int
-check_wav_header(char *header, int expected_sr)
-{
-    int sr;
-
-    if (header[34] != 0x10) {
-        E_ERROR("Input audio file has [%d] bits per sample instead of 16\n", header[34]);
-        return 0;
-    }
-    if (header[20] != 0x1) {
-        E_ERROR("Input audio file has compression [%d] and not required PCM\n", header[20]);
-        return 0;
-    }
-    if (header[22] != 0x1) {
-        E_ERROR("Input audio file has [%d] channels, expected single channel mono\n", header[22]);
-        return 0;
-    }
-    sr = ((header[24] & 0xFF) | ((header[25] & 0xFF) << 8) | ((header[26] & 0xFF) << 16) | ((header[27] & 0xFF) << 24));
-    if (sr != expected_sr) {
-        E_ERROR("Input audio file has sample rate [%d], but decoder expects [%d]\n", sr, expected_sr);
-        return 0;
-    }
-    return 1;
-}
-
-/*
- * Continuous recognition from a file
- */
-static void
-recognize_from_file()
-{
-    int16 adbuf[4096*2];
-    const char *fname;
-    const char *hyp;
-    int32 k;
-    uint8 utt_started, in_speech;
-    int32 print_times = cmd_ln_boolean_r(config, "-time");
-
-    fname = cmd_ln_str_r(config, "-infile");
-    if ((rawfd = fopen(fname, "rb")) == NULL) {
-        E_FATAL_SYSTEM("Failed to open file '%s' for reading",
-                       fname);
-    }
-    
-    if (strlen(fname) > 4 && strcmp(fname + strlen(fname) - 4, ".wav") == 0) {
-        char waveheader[44];
-	fread(waveheader, 1, 44, rawfd);
-	if (!check_wav_header(waveheader, (int)cmd_ln_float32_r(config, "-samprate")))
-    	    E_FATAL("Failed to process file '%s' due to format mismatch.\n", fname);
-    }
-    
-    ps_start_utt(ps);
-    utt_started = FALSE;
-
-    while ((k = fread(adbuf, sizeof(int16), 4096*2, rawfd)) > 0) {
-        ps_process_raw(ps, adbuf, k, FALSE, FALSE);
-        in_speech = ps_get_in_speech(ps);
-        if (in_speech && !utt_started) {
-            utt_started = TRUE;
-        } 
-        if (!in_speech && utt_started) {
-            ps_end_utt(ps);
-            hyp = ps_get_hyp(ps, NULL);
-            if (hyp != NULL)
-        	printf("%s\n", hyp);
-            if (print_times)
-        	print_word_times();
-
-            ps_start_utt(ps);
-            utt_started = FALSE;
-        }
-    }
-    ps_end_utt(ps);
-    if (utt_started) {
-        hyp = ps_get_hyp(ps, NULL);
-        if (hyp != NULL)
-    	    printf("%s\n", hyp);
-        if (print_times) {
-        print_word_times();
-	}
-    }
-    
-    fclose(rawfd);
-}
-
 /* Sleep for specified msec */
 static void
 sleep_msec(int32 ms)
 {
-#if (defined(_WIN32) && !defined(GNUWINCE)) || defined(_WIN32_WCE)
-    Sleep(ms);
-#else
     /* ------------------- Unix ------------------ */
     struct timeval tmo;
 
@@ -244,7 +70,6 @@ sleep_msec(int32 ms)
     tmo.tv_usec = ms * 1000;
 
     select(0, NULL, NULL, NULL, &tmo);
-#endif
 }
 
 /*
@@ -281,7 +106,7 @@ static int sStartRecord() {
     return pid;
 }
 
-static int sAplay(const char* file) {
+/*static int sAplay(const char* file) {
     pid_t pid = fork();
     if ( pid < 0) {
         return -1;
@@ -294,7 +119,7 @@ static int sAplay(const char* file) {
         _exit(1);
     }
     return pid;
-}
+}*/
 
 static int sAplayWait(const char* file) {
     pid_t pid = fork();
@@ -482,7 +307,7 @@ recognize_from_microphone()
                     bye = isBye(msg);
                     if ( !bye ) {
                         //output the command
-                        printf(msg);
+                        printf("%s", msg);
                         ad_stop_rec(ad);
                         sAplayWait(wav_proc);
                         ad_start_rec(ad);
@@ -526,8 +351,8 @@ main(int argc, char *argv[])
         config = cmd_ln_parse_file_r(config, cont_args_def, cfg, FALSE);
     }
 
-    if (config == NULL || (cmd_ln_str_r(config, "-infile") == NULL && cmd_ln_boolean_r(config, "-inmic") == FALSE)) {
-    E_INFO("Specify '-infile <file.wav>' to recognize from file or '-inmic yes' to recognize from microphone.");
+    if (config == NULL || (cmd_ln_str_r(config, "-cmdproc") == NULL)) {
+    E_INFO("Specify '-cmdproc to processing the speech file");
     cmd_ln_free_r(config);
     return 1;
     }
@@ -553,39 +378,10 @@ main(int argc, char *argv[])
 
     //disable stdout buffer
     setbuf(stdout, NULL);
-    if (cmd_ln_str_r(config, "-infile") != NULL) {
-        recognize_from_file();
-    } else if (cmd_ln_boolean_r(config, "-inmic")) {
-        recognize_from_microphone();
-    }
+    recognize_from_microphone();
 
     ps_free(ps);
     cmd_ln_free_r(config);
 
     return 0;
 }
-
-#if defined(_WIN32_WCE)
-#pragma comment(linker,"/entry:mainWCRTStartup")
-#include <windows.h>
-//Windows Mobile has the Unicode main only
-int
-wmain(int32 argc, wchar_t * wargv[])
-{
-    char **argv;
-    size_t wlen;
-    size_t len;
-    int i;
-
-    argv = malloc(argc * sizeof(char *));
-    for (i = 0; i < argc; i++) {
-        wlen = lstrlenW(wargv[i]);
-        len = wcstombs(NULL, wargv[i], wlen);
-        argv[i] = malloc(len + 1);
-        wcstombs(argv[i], wargv[i], wlen);
-    }
-
-    //assuming ASCII parameters
-    return main(argc, argv);
-}
-#endif
