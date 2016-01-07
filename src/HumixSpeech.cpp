4d62840c33b1e046498fff57055579401ab81aea
yhwang <yihongwang@gmail.com>
2016-01-07 23:31:40 +0800

    tweak sphinx parameters

--- src/HumixSpeech.cpp
+++ src/HumixSpeech.cpp
@@ -348,7 +348,7 @@ void HumixSpeech::sLoop(void* arg) {
             if (!_this->mAplayFiles.empty()) {
                 ad_stop_rec(ad);
                 std::string file = _this->mAplayFiles.front();
-                printf("receive aplay command:%s\n", file.c_str());
+                printf("play:%s\n", file.c_str());
                 {
                     WavPlayer player(file.c_str());
                     player.Play();
@@ -369,7 +369,7 @@ void HumixSpeech::sLoop(void* arg) {
                 humixCount = 0;
                 if (in_speech) {
                     _this->mState = kKeyword;
-                    printf("Waiting for keyward: HUMIX... \n");
+                    printf("Waiting for keyward: HUMIX...\n");
                 }
                 break;
             case kKeyword:
